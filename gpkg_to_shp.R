93b325141699b223503936d3b813c361eed42b72
‘admahood’ <admahood@gmail.com>
2021-11-11 16:06:19 -0700

    j

--- gpkg_to_shp.R
+++ gpkg_to_shp.R
@@ -1,15 +1,16 @@
 # function to convert a gpkg to a shp
 
-gpkg_to_shp <- function(x, type = "shp"){
+gpkg_to_shp_to_zip <- function(path, file, type = "shp"){
   require(sf)
-  input <- st_read(x)
+  input <- st_read(file.path(path, file))
   st_write(input, str_replace(x, "gpkg", type), delete_dsn=TRUE)
-  pattern = str_extract(x, "fired_[a-z]+")
-  datepat = str_extract(x, "to\\d{7}")
-  setwd("/home/a/Desktop/FIRED_stuff/")
-  zfiles <- list.files("/home/a/Desktop/FIRED_stuff", pattern = pattern)
-  zip(zipfile= paste0("/home/a/Desktop/FIRED_stuff/", pattern,"_", datepat, "_gpkg_shp.zip" ),
+  pattern = str_extract(file, "fired_[a-z]+")
+  datepat = str_extract(file, "to\\d{7}")
+  setwd(path)
+  zfiles <- list.files(path, pattern = pattern)
+  zip(zipfile= paste0(path,"/", pattern,"_", datepat, "_gpkg_shp.zip" ),
       files = zfiles)
 }
 
-gpkg_to_shp("/home/a/Desktop/FIRED_stuff/fired_benin_to2021182_events.gpkg")
+gpkg_to_shp_to_zip(path="/home/a/Desktop/FIRED_stuff",
+            file="fired_victoria_to2021182_events.gpkg")
