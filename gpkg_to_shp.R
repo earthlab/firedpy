c9b81b5eb3695bc74db09c6970145d992b2b36ae
‘admahood’ <admahood@gmail.com>
2021-11-12 09:46:09 -0700

    j

--- gpkg_to_shp.R
+++ gpkg_to_shp.R
@@ -2,10 +2,15 @@
 
 gpkg_to_shp_to_zip <- function(path, file, type = "shp"){
   require(sf)
+  require(tidyverse)
   input <- st_read(file.path(path, file))
-  st_write(input, str_replace(x, "gpkg", type), delete_dsn=TRUE)
+  st_write(input, str_replace(file.path(path, file), "gpkg", type), delete_dsn=TRUE)
   pattern = str_extract(file, "fired_[a-z]+")
-  datepat = str_extract(file, "to\\d{7}")
+  datestr = str_extract(file, "\\d{7}") %>% as.Date("%Y%j")
+  datepat = paste("to",
+                  lubridate::month(datestr, label=TRUE, abbr=FALSE),
+                  lubridate::year(datestr),
+                  sep="_")
   setwd(path)
   zfiles <- list.files(path, pattern = pattern)
   zip(zipfile= paste0(path,"/", pattern,"_", datepat, "_gpkg_shp.zip" ),
@@ -13,4 +18,4 @@ gpkg_to_shp_to_zip <- function(path, file, type = "shp"){
 }
 
 gpkg_to_shp_to_zip(path="/home/a/Desktop/FIRED_stuff",
-            file="fired_victoria_to2021182_events.gpkg")
+            file="fired_sudan_to2021182_events.gpkg")
