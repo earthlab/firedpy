bb8f993d4b1826b5365dc7fa7c0ed26f76e58134
‘admahood’ <admahood@gmail.com>
2021-11-17 16:37:23 -0700

    updated map

--- map_figure.R
+++ map_figure.R
@@ -16,6 +16,7 @@ completed_countries <-c(filter(world, CONTINENT == "North America") %>% pull(NAM
                         "ghana","togo", "benin", "niger","burkina_faso", "nigeria",
                         "colombia", "ecuador", "peru", "venezuela", "sudan",
                         "cameroon", "gabon", "chad", "japan", "north_korea",
+                        "uganda", "kenya",
                         "malaysia", "brunei", "vietnam", "thailand", "indonesia",
                         "south_korea", "taiwan", "philippines", "papua_new_guinea",
                         "east_timor", "new_zealand", "nepal", "bhutan", "bangladesh",
