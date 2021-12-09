3c5feec8af902d2e19ae9fb0cbdceb22850f3135
‘admahood’ <admahood@gmail.com>
2021-12-09 10:53:34 -0700

    finished drc

--- R/gpkg_to_shp.R
+++ R/gpkg_to_shp.R
@@ -18,7 +18,7 @@ gpkg_to_shp_to_zip <- function(path, file, type = "shp"){
 }
 
 gpkg_to_shp_to_zip(path="/home/a/Desktop/FIRED_stuff",
-            file="fired_zambia_to2021182_events.gpkg")
+            file="fired_democratic_republic_of_the_congo_to2021182_events.gpkg")
 
 
 # the_path <- "/home/a/Desktop/FIRED_stuff/upload_to_drive"
