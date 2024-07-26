e53acf00d2c543ef5f2bc754ea62aa04eeb3e3e0
earthlab <erve3705@colorado.edu>
2024-07-26 12:25:07 -0600

    Updating environment.yml

--- bin/process_all_countries.py
+++ bin/process_all_countries.py
@@ -8,7 +8,13 @@ if __name__ == '__main__':
     countries = [c.strip('.gpkg') for c in os.listdir(os.path.join(PROJECT_DIR, 'ref', 'individual_countries'))]
 
     for country in countries:
+        print(country)
+        if country == "enya":
+            country = "kenya"
+        if country in ['mozambique', 'faroe_islands', 'western_sahara', 'french_polynesia']:
+            continue
         if not any([country in f for f in os.listdir(os.path.join(PROJECT_DIR, 'output', 'outputs'))]):
+            print("running", country)
             process = sp.Popen([
                 sys.executable,
                 os.path.join(PROJECT_DIR, 'bin', 'firedpy.py'),
@@ -17,8 +23,8 @@ if __name__ == '__main__':
                 "--tile_choice", "b",
                 "--tile_name", country,
                 "--daily", "y",
-                "-spatial", "5",
-                "-temporal", "11",
+                "-spatial", "1",
+                "-temporal", "5",
                 "-shape_type", "both",
                 "-land_cover_type", "1",
                 "--cleanup", "y",
@@ -26,3 +32,5 @@ if __name__ == '__main__':
                 "-end_year", "0"
             ])  # Adjust shell=True if needed
             return_code = process.wait()  # Wait for process completion
+        else:
+            print('Skipping', country)
