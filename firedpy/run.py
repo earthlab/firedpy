196bf2bffc4dda702fd2a67dd6bf98060e30219f
twillico <twillico@proton.me>
2026-03-15 11:45:20 -0600

    Fixed big indexing error in EventGrid.get_event_perimeters.

--- firedpy/run.py
+++ firedpy/run.py
@@ -278,23 +278,3 @@ def fired(
         cleanup_intermediate_files(project_directory)
 
     return gdf
-
-
-if __name__ == "__main__":
-    tiles = "h09v05"
-    daily = True
-    full_csv = True
-    project_directory = '/home/travis/scratch/firedpy/rockies_test'
-    project_name = 'rockies_test'
-    n_cores = 1
-    start_year = 2023
-    country = None
-    shape_file = None
-    end_year = 2025
-    spatial_param = 8  # pixels (nominally ~3,704 m but varies by location)
-    temporal_param = 3  # days
-    shape_type = 'gpkg'  # GeoPackage
-    eco_region_level = None  # Level I - Least Detailed
-    eco_region_type = 'na'  # North American Ecoregions (Omernick, 1987)
-    land_cover_type = None  # International Geosphere-Biosphere Programme (IGBP) scheme
-    cleanup = False
